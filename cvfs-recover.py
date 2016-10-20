d5f209154ac76112f857989b659c269ebeb2b8fd
Bengt Ove Sannes <bengtove@hocusfocus.no>
2016-10-20 21:27:35 +0200

    Added support for .gz index files

--- cvfs-recover.py
+++ cvfs-recover.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-import sys, os, re
+import sys, os, re, struct, gzip
+
+def getuncompressedsize(filename):
+    with open(filename, 'rb') as f:
+        f.seek(-4, 2)
+        return struct.unpack('I', f.read(4))[0]
 
 usage = """Usage:
 recover.py [-n] [--limit=0] directory_with_FOUND_files volume_index_file
-
-or from an existing log file (made with -n):
-recover.py -p [recover.log]"""
+"""
 
 found_files = {}
 i = 0
@@ -33,7 +36,7 @@ for arg in sys.argv[1:]:
 				full_path = os.path.join(arg, name)
 				inode_number = os.stat(full_path).st_ino
 				found_files[inode_number] = full_path
-				print  'FOUND* count: %i\r' % i,
+				print  'Orphan count: %i\r' % i,
 				if i == params['limit']:
 					print '\nReached limit\r',
 					break
@@ -45,6 +48,7 @@ for arg in sys.argv[1:]:
 
 if len(logs) == 0 and ( len(indexes) == 0 or len(found_files) == 0 ):
 	print usage
+	sys.exit(1)
 
 print '\n'
 
@@ -52,13 +56,23 @@ lognr = 0
 while os.path.isfile(params['log'] % lognr):
 	lognr += 1
 logfile = params['log'] % lognr
-
+restored_count = 0
+error_count = 0
 for index in indexes:
+	zipped = False
 	print 'Looking for matches in ' + index
-	index_size = os.path.getsize(index)
+	if index.endswith('.gz'):
+		zipped = True
+		print index+' is zipped. Switching to gzip mode.'
+		index_size = getuncompressedsize(index)
+		f = gzip.open(index)
+	else:
+		index_size = os.path.getsize(index)
+		f = open(index)
 	parsed_bytes = 0
 	print 'Index size: %i' % index_size
-	for line in open(index):
+
+	for line in f:
 		parsed_bytes += len(line)
 		#line_split = line.strip().split()
 		#print repr(line)
@@ -67,18 +81,29 @@ for index in indexes:
 		inode_number = int(line_split[0])
 		stored_path = volume_root+line_split[-1].decode('string_escape')
 		if inode_number in found_files and not os.path.exists(stored_path):
+			parent_dir = os.path.dirname(stored_path)
+			if not os.path.isdir(parent_dir):
+				try:
+					os.makedirs(parent_dir)
+					loglinedir = 'Created missing directory: %s\n' % parent_dir
+				except OSError as e:
+					loglinedir = ('Could not create missing directory: %s ' % parent_dir)+("Error({0}): {1}\n".format(e.errno, e.strerror))
+				open(logfile, 'a').write(loglinedir)
+				print loglinedir,
 			logline = ('Inode: %i ' % inode_number).ljust(20)+'%s > %s\n' % (found_files[inode_number], stored_path)
-			print logline,
 			if not no_apply:
 				try:
 					os.rename(found_files[inode_number], stored_path)
-					print 'Moved'
 					logline = 'Restored: '+logline
+					restored_count += 1
 				except OSError as e:
 					logline = "Error({0}): {1} > ".format(e.errno, e.strerror)+logline
-					print "I/O error({0}): {1}".format(e.errno, e.strerror)
+					error_count += 1
+			print '\r'+logline,
 			open(logfile, 'a').write(logline)
 		progress = float(parsed_bytes) / float(index_size)
 		print  '\r%05.2f%%' % (progress * 100.0),
 
+logline = 'Orphans detected: %i Restored: %i Errors: %i Still nameless: %i\n' % (i, restored_count, error_count, i - restored_count)
+print '\n'+logline,
 print 'Log file: ' + logfile
